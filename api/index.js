0a0051847de39a04580c8b4503328451b8e16303
imxkamil <marszalkowski.kamil01@gmail.com>
2024-03-14 18:44:55 +0100

    connected mongo db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('you are connected to db');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
 
 const app = express();
 
