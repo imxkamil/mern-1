b32e7c01dfa10f46661fdd4b87bce764c6b669a1
imxkamil <marszalkowski.kamil01@gmail.com>
2024-03-16 12:57:24 +0100

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 
@@ -20,4 +21,6 @@ app.listen(3000, ()=> {
     }
 );
 
+
+app.use('/api/user', userRouter);
 
