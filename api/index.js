9c20450fb7bf636df1fe23ad1530f7df2ca4870c
imxkamil <marszalkowski.kamil01@gmail.com>
2024-03-18 14:20:27 +0100

    added backend logic for sign up

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 
@@ -16,6 +17,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, ()=> {
     console.log('server is running on port 3000');
     }
@@ -23,4 +26,5 @@ app.listen(3000, ()=> {
 
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
